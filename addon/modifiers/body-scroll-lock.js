ac04245d088e09fef94f6258406babcf4dc00b6a
Steve <Steve.Hummingbird@yandex.com>
2021-02-28 18:11:17 +0100

    Update body-scroll-lock.js
    do not run in fastboot mode

--- addon/modifiers/body-scroll-lock.js
+++ addon/modifiers/body-scroll-lock.js
@@ -1,7 +1,13 @@
 import { modifier } from 'ember-modifier';
+import { getOwner } from '@ember/application';
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 export default modifier(function bodyScrollLock(element, _, options) {
+  const owner = getOwner(this);
+  const fastboot = owner.lookup('service:fastboot');
+  if (fastboot && fastboot.isFastBoot) {
+    return;
+  }
   disableBodyScroll(element, options);
   return () => enableBodyScroll(element);
 });
